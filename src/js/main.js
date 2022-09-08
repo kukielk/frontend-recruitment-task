cd16a1bcf646f90d6c8e199f4cbbf26a4c5e04ac
kukielk <aleksandra.kukielka25@gmail.com>
2022-09-09 00:53:10 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,48 @@
-let button = document.getElementById("button");
+const openBtn = document.querySelector('#btn');
+const closeBtn = document.querySelector('#popup_c_btn');
+const alertPopup = document.querySelector('#popUp');
+const alertShadow = document.querySelector('#section_shadow');
 
-function Click(){
-    alert("Alert!");
-    alert("Kliknąłeś X razy!");
+
+const clickCounter = document.querySelector('#kliknmb');
+
+
+let clickBtnNumb = 0;
+let browserStorage = JSON.parse(window.localStorage.getItem('click'));
+let resetBtn = document.createElement('button');
+let addButtonFlag = true;
+
+openBtn.addEventListener('click', () => {
+    alertPopup.classList.add('active');
+    alertShadow.classList.add('active');
+    clickBtnNumb++;
+    window.localStorage.setItem('click', `${browserStorage}`);
+    clickCounter.textContent = ++browserStorage;
+
+    if (browserStorage >= 5) {
+        if (addButtonFlag = true) {
+            alertPopup.appendChild(resetBtn);
+            resetBtn.textContent = "Reset Click Number";
+            resetBtn.classList.add('popup_resetBtn');
+            addButtonFlag = false;
+        }
+        resetBtn.addEventListener('click', () => {
+            clickBtnNumb = 0;
+            browserStorage = 0;
+            window.localStorage.setItem('click', `${browserStorage}`);
+            clickCounter.textContent = 0;
+            alertPopup.removeChild(resetBtn);
+        })
+
+
+    };
+});
+
+closeBtn.addEventListener('click', closePopup);
+
+alertShadow.addEventListener('click', closePopup);
+
+function closePopup() {
+    alertPopup.classList.remove('active');
+    alertShadow.classList.remove('active');
 }
-let clicks = 0;
-
-function klik(){
-    clicks += 1;
-    button.innerHTML = clicks;
-};
-
-button.onclick = Click;
